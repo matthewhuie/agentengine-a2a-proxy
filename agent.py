8e6c695ef089daed92275eeb88815e7a6da2432d
Matthew Huie <matthew@matthewhuie.com>
2026-01-27 18:33:35 -0500

    Starter code for RemoteA2aAgent

--- agent.py
+++ agent.py
@@ -1,1 +1,8 @@
-from google.adk.agents import Agent
+from google.adk.agents.remote_a2a_agent import AGENT_CARD_WELL_KNOWN_PATH
+from google.adk.agents.remote_a2a_agent import RemoteA2aAgent
+
+root_agent = RemoteA2aAgent(
+    name="proxy_agent",
+    description="Agent that proxies requests to an agent on an A2A server.",
+    agent_card="https://A2A_AGENT_URL{AGENT_CARD_WELL_KNOWN_PATH}",
+)
